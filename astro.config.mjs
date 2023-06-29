bbbfbbca3c6de0a0225cd971ec23ce4466e4862e
Yumi Izumi <me@yumiizumi.com>
2023-06-30 01:02:21 +0900

    Add compress integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://onekiji.com/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), compress()]
 });
