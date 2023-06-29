3a6b77ce9e7a5f096fda5ad90cb94f71df49ad79
Yumi Izumi <me@yumiizumi.com>
2023-06-29 17:01:39 +0900

    Initial commit with Astro

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from "astro:content";
+
+const kiji = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { kiji };
