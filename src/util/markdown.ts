26f34a92824caa2baaa531fc341f9cca79517573
Yumi Izumi <me@yumiizumi.com>
2023-07-01 15:53:10 +0900

    Add reading time

--- src/util/markdown.ts
+++ src/util/markdown.ts
@@ -9,3 +9,11 @@ export function getTitleMd(markdown: string) {
 export function getFirstParagraphMd(markdown: string) {
   return breakMd(markdown)[1];
 }
+
+export function readingTimeMd(markdown: string) {
+  const words = breakMd(markdown).reduce((acc, curr) => {
+    return acc + curr.split(" ").length;
+  }, 0);
+  const minutes = Math.ceil(words / 200); // 200 words per minute is the average reading speed
+  return minutes;
+}
