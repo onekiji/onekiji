1adf87cf0d74af1880120b8a37d2c08b036b90bf
Yumi Izumi <me@yumiizumi.com>
2023-07-10 19:35:30 +0900

    Sort RSS feed

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -2,12 +2,27 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-import { filenameToDateString, getFirstParagraphMd, getTitleMd } from "@/util";
+import {
+  filenameToDateString,
+  getFirstParagraphMd,
+  getTitleMd,
+  languageFromFilename,
+} from "@/util";
 import { localizePath } from "astro-i18next";
 const parser = new MarkdownIt();
 
 export async function get(context) {
-  const kijis = await getCollection("kiji");
+  const kijis = await getCollection(
+    "kiji",
+    ({ slug }) => languageFromFilename(slug) === "en"
+  ).then((posts) =>
+    posts.sort((a, b) =>
+      new Date(filenameToDateString(b.slug)) >
+      new Date(filenameToDateString(a.slug))
+        ? 1
+        : -1
+    )
+  );
   return rss({
     title: "One Kiji",
     description:
